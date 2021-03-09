38f9451b555b306349d2f854bc26eafd2b909966
Manolo Muñoz <manolo@closemarketing.net>
2021-03-09 14:26:20 +0100

    change to subfiles

---
+++ includes/class-admin.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Class for admin.
+ *
+ * Description.
+ *
+ * @since 1.0
+ */
+class WCES_Admin {
+
+    /**
+     * Construct of Class
+     */
+    public function __construct() {
+        
+    }
+
+    /**
+     * # Functions
+     * ---------------------------------------------------------------------------------------------------- */
+}
+
+new WCES_Admin;
