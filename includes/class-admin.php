c6cf7375106f6e6b065348cee0fc345b12c62868
David Perez <david@closemarketing.es>
2021-03-10 14:39:11 +0100

    arreglos phpcs

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -9,35 +9,30 @@
  */
 class WCES_Admin {
 
-    /**
-     * Construct of Class
-     */
-    public function __construct() {
-        // add the action 
-        add_action ( 'admin_notices' , array( $this, 'action_admin_notices' ) );
-        
-    }
+	/**
+	 * Construct of Class
+	 */
+	public function __construct() {
+		// Admin notice.
+		add_action( 'admin_notices', array( $this, 'action_admin_notices' ) );
+	}
 
-    /**
-     *  define the admin_notices callback
-     * */
-    function action_admin_notices() {
-        ?>
-        <div class="notice notice-success is-dismissible">
-            <p><?php _e( '<img width="20" height="20" src="https://www.flaticon.es/svg/vstatic/svg/3522/3522675.svg?token=exp=1615373953~hmac=0224b5ab4e39ec52539818bed35036df"></img><img width="20" height="20" src="https://www.flaticon.es/svg/vstatic/svg/3522/3522283.svg?token=exp=1615374581~hmac=e5e7152686ffb169efdbc0cca4d23817"></img>
-            ¡Subscribe to WooCommerce!<button type="submit" name="btnSub" id="btnSub" onclick=\'alert("Hello")\'>¡Subscribe!</button>', 'sample-text-domain' ); ?></p>
-        </div>
-        <?php
-        
-    }
-
-    /**
-     * Change variable so that it does not appear again
-     */
-    function change_dismiss()
-    {
-        $dismiss=true;
-    }
+	/**
+	 * Define de admin notices
+	 *
+	 * @return void
+	 */
+	public function action_admin_notices() {
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p>
+				<?php
+				esc_html_e( '¡Subscribe to WooCommerce!<button type="submit" name="btnSub" id="btnSub" onclick=\'alert("Hello")\'>¡Subscribe!</button>', 'woocommerce-es' );
+				?>
+			</p>
+		</div>
+		<?php
+	}
 }
 
-new WCES_Admin;
+new WCES_Admin();
