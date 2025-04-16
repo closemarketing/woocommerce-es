fb369f6056c5042c292a69fb2a22feb58bf5a7fd
davidperezgar <david@closemarketing.es>
2025-04-16 13:12:16 +0200

    beta

--- connect-ecommerce.php
+++ connect-ecommerce.php
@@ -5,7 +5,7 @@
  * Description: Connects Ecommerce WooCommerce to ERPs and CRMs. Syncs products, customers, orders and stock.
  * Author: Closetechnology
  * Author URI: https://close.technology/
- * Version: 3.0.0
+ * Version: 3.0.0-beta.1
  *
  * @package WordPress
  * Text Domain: connect-ecommerce
@@ -15,7 +15,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'CONECOM_VERSION', '3.0.0' );
+define( 'CONECOM_VERSION', '3.0.0-beta.1' );
 define( 'CONECOM_FILE', __FILE__ );
 define( 'CONECOM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CONECOM_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
