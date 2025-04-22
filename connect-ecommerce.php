27905bef947852f927e804d39d1c02ee9b0ecb42
davidperezgar <david@closemarketing.es>
2025-04-22 08:46:05 +0200

    woocommerce header

--- connect-ecommerce.php
+++ connect-ecommerce.php
@@ -9,6 +9,7 @@
  *
  * @package WordPress
  * Text Domain: connect-ecommerce
+ * Requires Plugins: woocommerce
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
