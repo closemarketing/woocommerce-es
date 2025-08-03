8716cd7093552f10659a574f404f06cc48762053
davidperezgar <david@closemarketing.es>
2025-08-03 12:50:24 +0200

    clean

--- tests/WooCommerce/test-create-product-simple.php
+++ tests/WooCommerce/test-create-product-simple.php
@@ -44,7 +44,7 @@ class CreateProductSimpleTest extends WP_UnitTestCase {
 	public function test_create_product_simple_without_errors() {
 		$item_path = UNIT_TESTS_DATA_PLUGIN_DIR . 'Products/simple.json';
 		$item      = file_get_contents( $item_path );
-		$item      = json_decode( $item, true );
+		$item      = json_decode( $item, true )[0];
 		
 		$result_sync = PROD::sync_product_item( $this->settings, $item, $this->connapi_erp );
 		
