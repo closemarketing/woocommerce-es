a4322798807a01694300c01fb87172bba687a936
davidperezgar <david@closemarketing.es>
2025-08-04 12:56:00 +0200

    requires 7.4

--- connect-ecommerce.php
+++ connect-ecommerce.php
@@ -9,6 +9,7 @@
  *
  * @package WordPress
  * Text Domain: connect-ecommerce
+ * Requires PHP: 7.4
  * Requires Plugins: woocommerce
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
