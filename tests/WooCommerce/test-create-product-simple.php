d6b9788211460c027de2926c3aa90811ea869626
davidperezgar <david@closemarketing.es>
2025-08-04 12:44:17 +0200

    more asserts simple product

--- tests/WooCommerce/test-create-product-simple.php
+++ tests/WooCommerce/test-create-product-simple.php
@@ -77,20 +77,43 @@ class CreateProductSimpleTest extends WP_UnitTestCase {
 	 * Create Product Simple without Errors.
 	 */
 	public function test_create_product_simple_without_errors() {
-		$item_path = UNIT_TESTS_DATA_PLUGIN_DIR . 'Products/simple.json';
+		$item_path = UNIT_TESTS_DATA_PLUGIN_DIR . 'product-simple.json';
 		$item      = file_get_contents( $item_path );
 		$item      = json_decode( $item, true )[0];
 		
-		$result_sync = PROD::sync_product_item( $this->settings, $item, $this->connapi_erp );
+		$result_sync    = PROD::sync_product_item( $this->settings, $item, $this->connapi_erp );
+		$result_prod_id = $result_sync['post_id'];
 		
 		$this->assertNotNull( $result_sync );
 		$this->assertEquals( 'ok', $result_sync['status'] );
-		$this->assertIsInt( $result_sync['post_id'] );
+		$this->assertIsInt( $result_prod_id );
 		
-		$product = wc_get_product( $result_sync['post_id'] );
+		$product = wc_get_product( $result_prod_id );
 		$this->assertInstanceOf( 'WC_Product_Simple', $product );
 		$this->assertEquals( $item['sku'], $product->get_sku() );
+		$this->assertEquals( $item['barcode'], $product->get_global_unique_id() );
 
-		wp_delete_post( $result_sync['post_id'], true ); // Clean up after test
+		// Update product asserts.
+		$update_post = [
+			'ID'           => $result_prod_id,
+			'post_title'   => 'Updated Product Title',
+			'post_content' => 'Updated product description.',
+		];
+		wp_update_post( $update_post );
+		$result_sync_upd = PROD::sync_product_item( $this->settings, $item, $this->connapi_erp, false, $result_prod_id );
+
+		$this->assertNotNull( $result_sync_upd );
+		$this->assertEquals( 'ok', $result_sync_upd['status'] );
+		$this->assertIsInt( $result_sync_upd['post_id'] );
+		$this->assertEquals( $result_prod_id, $result_sync_upd['post_id'] );
+
+		// Prices.
+		$this->assertEquals( $item['price'], get_post_meta( $result_sync_upd['post_id'], '_regular_price', true ) );
+
+		// Product update does not change Title and Content.
+		$this->assertEquals( 'Updated Product Title', get_the_title( $result_sync_upd['post_id'] ) );
+		$this->assertEquals( 'Updated product description.', get_post_field( 'post_content', $result_sync_upd['post_id'] ) );
+
+		wp_delete_post( $result_sync_upd['post_id'], true ); // Clean up after test
 	}
 }
