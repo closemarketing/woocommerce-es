0c86ea6a68391d6e742a4eea27899a21b6672c22
davidperezgar <david@closemarketing.es>
2025-08-05 16:47:50 +0200

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,15 @@
 define( 'TESTS_PLUGIN_DIR', dirname( __DIR__ ) );
 define( 'UNIT_TESTS_DATA_PLUGIN_DIR', TESTS_PLUGIN_DIR . '/tests/Data/' );
 
+// Define WP_CORE_DIR if not already defined
+if ( ! defined( 'WP_CORE_DIR' ) ) {
+	$_wp_core_dir = getenv( 'WP_CORE_DIR' );
+	if ( ! $_wp_core_dir ) {
+		$_wp_core_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress';
+	}
+	define( 'WP_CORE_DIR', $_wp_core_dir );
+}
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
