a77df1f5c675a0945537dc538d21e8b2a5115a8b
Alex Brea <alex@close.marketing>
2026-04-01 11:39:20 +0200

    change || by &&

--- includes/Helpers/PROD.php
+++ includes/Helpers/PROD.php
@@ -783,16 +783,17 @@ class PROD {
 		}
 
 		// Filter by tags.
-		if ( empty( $settings['filter'] ) || empty( $item['tags'] ) ) {
+		if ( empty( $settings['filter'] ) && empty( $item['tags'] ) ) {
 			return false;
 		}
+
 		$tags_option = explode( ',', $settings['filter'] );
 		$tags_option = array_map( 'trim', $tags_option );
 		$tags_option = array_map( 'sanitize_text_field', $tags_option );
 
-		$tags_prod = array_map( 'trim', $item['tags'] );
-		$tags_prod = array_map( 'sanitize_text_field', $tags_prod );
-		$tags_prod = array_filter( $tags_prod );
+		$tags_prod   = array_map( 'trim', $item['tags'] );
+		$tags_prod   = array_map( 'sanitize_text_field', $tags_prod );
+		$tags_prod   = array_filter( $tags_prod );
 
 		return empty( array_intersect( $tags_option, $tags_prod ) ) ? true : false;
 	}
