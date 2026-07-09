aa7cafaece8eb2132a002f5377b32b59d717d851
davidperezgar <david@closemarketing.es>
2026-07-09 08:40:28 +0200

    Always inherit tax status/class from the parent product.

--- includes/Helpers/PROD.php
+++ includes/Helpers/PROD.php
@@ -609,6 +609,8 @@ class PROD {
 				'parent_id'     => $product_id,
 				'attributes'    => $attributes_prod,
 				'regular_price' => $variation_price,
+				'tax_status'    => 'taxable',
+				'tax_class'     => 'parent',
 			);
 
 			$price_sale = self::get_sale_price( $variant, $settings );
@@ -618,8 +620,6 @@ class PROD {
 			if ( 0 === $variation_id ) {
 				// New variation.
 				$variation_props_new = array(
-					'tax_status'   => 'taxable',
-					'tax_class'    => 'parent',
 					'weight'       => '',
 					'length'       => '',
 					'width'        => '',
